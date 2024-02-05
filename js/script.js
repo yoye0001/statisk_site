3e553db74aee08649aca21679b66aa531fcc5e89
youhan <yoye0001@stud.kea.dk>
2024-02-05 21:48:10 +0100

    1.st

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const url = "https://kea-alt-del.dk/t7/api/products";
+
+https: fetch(url)
+  .then((response) => response.json())
+  .then(dataRecieved);
+
+function dataRecieved(data) {
+  console.log(data);
+  data.forEach(listProducts);
+}
+
+function listProducts(oneProduct) {
+  console.log("listProducts");
+  const product = document.querySelector("template").content;
+  const myClone = product.cloneNode(true);
+
+  myClone.querySelector(".productdisplayname").textContent = oneProduct.productdisplayname;
+  myClone.querySelector(".gender").textContent = oneProduct.gender;
+  myClone.querySelector(".price").textContent = oneProduct.price;
+
+  myClone.querySelector(".articletype").textContent = oneProduct.articletype;
+
+  const parentElement = document.querySelector("main");
+  parentElement.appendChild(myClone);
+}
